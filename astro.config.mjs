3f3fc33ade4b5ffaa26a5e4d3130716f0755bb37
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-01-21 17:36:53 -0800

    :zap: astro compress
    added astro-compress utility to lower bundle size

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), compress()]
 });
