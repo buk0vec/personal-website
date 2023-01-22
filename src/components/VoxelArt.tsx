37aa544601dd17956b9f4aa8887471ff60801c24
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-01-21 22:58:36 -0800

    :sparkles: added loading spinner

--- src/components/VoxelArt.tsx
+++ src/components/VoxelArt.tsx
@@ -1,17 +1,31 @@
 import { Suspense, lazy } from "react";
-import {
-  Html,
-  useProgress,
-  SpotLight
-} from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
-const Canvas = lazy(() => import('./ThreeCanvas'))
-const Model = lazy(() => import('./CV_Center'))
-const OrbitControls = lazy(() => import('./OrbitControls'))
+const Canvas = lazy(() => import("./ThreeCanvas"));
+const Model = lazy(() => import("./CV_Center"));
+const OrbitControls = lazy(() => import("./OrbitControls"));
 
 function Loader() {
-  const { progress } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
+  return (
+    <Html center as="div">
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            dur="0.75s"
+            values="0 12 12;360 12 12"
+            repeatCount="indefinite"
+          />
+        </path>
+      </svg>
+    </Html>
+  );
 }
 
 const VoxelArt = () => {
@@ -24,7 +38,7 @@ const VoxelArt = () => {
         <pointLight position={[-200, 40, 50]} castShadow={true} />
       </Suspense>
     </Canvas>
-  ); 
+  );
 };
 
 export default VoxelArt;
