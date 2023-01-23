de7805a1e393e57ea570d61d52b66d28a23818ad
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-01-22 16:56:56 -0800

    :bug: es6 + loading flash
    ahhhh

--- src/components/DarkToggle.tsx
+++ src/components/DarkToggle.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { TbMoonStars } from "react-icons/tb";
-import { IoSunnyOutline } from "react-icons/io5";
+import { TbMoonStars } from "react-icons/tb/index.js";
+import { IoSunnyOutline } from "react-icons/io5/index.js";
 
 const DarkToggle = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+  const [loaded, setLoaded] = useState(false);
 
   const handleClick = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -18,14 +19,24 @@ const DarkToggle = () => {
     localStorage.setItem("theme", theme);
   }, [theme]);
 
+  useEffect(() => {
+    setLoaded(true)
+  }, []);
+
+  if (!loaded) {
+  }
+  
   return (
     <button className="h-fit" onClick={handleClick}>
       <div className="p-1 rounded-md bg-stone-300 dark:bg-stone-700 hover:outline outline-2 outline-black dark:outline-neutral-400">
-        {theme === "light" ? (
-          <TbMoonStars size={"1.5em"} className="stroke-black" />
-        ) : (
-          <IoSunnyOutline size={"1.5em"} className="stroke-neutral-400" />
-        )}
+        {
+          loaded ? (theme === "light" ? (
+            <TbMoonStars size={"1.5em"} className="stroke-black" />
+          ) : (
+            <IoSunnyOutline size={"1.5em"} className="stroke-neutral-400" />
+          )) : <div className="h-6 w-6"></div>
+        }
+
       </div>
     </button>
   );
