305751f21a3edceac9c67448e836f809efe86c79
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-01-26 18:37:21 -0800

    :chart_with_upwards_trend: vercel analytics + partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,9 +22,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://www.bukovec.dev/",
   integrations: [tailwind(), react(), compress(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), prefetch(), mdx(), sitemap()]
+  }), prefetch(), mdx(), sitemap(), partytown()]
 });
